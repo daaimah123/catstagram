163bc38db2f033f20702150e400b087a4f56c7c7
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-04-15 09:28:21 -0700

    add links to page routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@
 	// include them inside of App
 	// export the App component!
 
-// Exercise 6: Routing
+// TODO: Exercise 6: Routing
 // Allow your user to route to different pages!
 	// need to import BrowserRouter and Router from react-router-dom
 	// Create basic class components on AboutPage and Profile Page, export them
@@ -14,16 +14,21 @@
 	// set up Route to with paths to each page (hint: HomePage should "/") and with the appropriate component
 
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import AboutPage from './pages/AboutPage.jsx';
 import Header from './components/Header.jsx';
 import HomePage from './pages/HomePage.jsx';
 
 class App extends React.Component {
     render() {
         return (
+		<BrowserRouter>
             <div> 
                 <Header />
-                <HomePage />
+                <Route exact path="/" component={HomePage} />
+				<Route path="/about" component={AboutPage} />
             </div>
+		</BrowserRouter>
         );
     }
 }
