4ccdac7477bb9ce53d317030eb5c1415dd0aa987
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-04-15 09:20:09 -0700

    add routes to page components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,25 @@
     // put class name of Logo on the li element with GDInsta
     // export default the component!
 
-// Exercise 6: Routing
+//TODO: Exercise 6: Routing
     // import Link from react-router-dom
     // use the Link component to link to each route with a 'to' property
     // check that your links work!
 
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
         return (
             <ul className="Header">
                 <li className="Logo">Catstagram </li>
-                <li>Home </li>
-                <li>About </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
                 <li>Profile </li>
             </ul>
         );
